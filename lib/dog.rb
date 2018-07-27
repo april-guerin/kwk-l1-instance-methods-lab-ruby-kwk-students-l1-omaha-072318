b7f9af1ae9f1d6b2a05e6baec8836a6c0ad4b1e3
april-guerin <aprilguerin@yahoo.com>
2018-07-27 16:52:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # dog.rb
+class Dog
+  def bark(bark)
+    @bark = bark
+  end
+  def bark
+    puts "Woof!"
+  end
+  
+  def sit(sit)
+    @sit = sit
+  end
+  def sit
+    puts "The Dog is sitting"
+  end
+end
